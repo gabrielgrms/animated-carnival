cabb3371efff1863999374b7d7f974f1c8563e55
Guli <gabrielgoulart@infojr.com.br>
2017-04-07 17:28:18 -0300

    Adicionando funcao luta

--- script.js
+++ script.js
@@ -5,10 +5,38 @@ function compare(){
     var second = prompt("Armadura");
     var gg = document.getElementById("resultado");
     if(first > second){
-        gg.innerHTML = "First";
+        gg.innerHTML = "FIRST";
         console.log("first");
     }
     else{
+        gg.innerHTML = "SECOND";
         console.log("second");
     }
+}
+
+function fight(){
+    var char = "Lutador 1";
+    var char2 = "Lutador 2";
+    var forca1 = Math.ceil(Math.random()*6);
+    var forca2 = Math.ceil(Math.random()*6);
+    var armor1 = Math.ceil(Math.random()*6);
+    var armor2 = Math.ceil(Math.random()*6);
+    var HP1 = armor1 * 5;
+    var HP2 = armor2 * 5;
+    var Currenthp1 = HP1;
+    var Currenthp2 = HP2;
+
+    while(Currenthp1>0 && Currenthp2>0){
+        Currenthp1 -= forca2*Math.random(1,6) - armor1*Math.random(1,6);
+        Currenthp2 -= forca1*Math.random(1,6) - armor2*Math.random(1,6);
+    }
+    if(HP1 > 0){
+        console.log("Nome: ", char, "\n", "Força: ", forca1, "\n", "Armadura: ", armor1, "\n", "HP: " ,Currenthp1, "\n", "Nome: ", char2, "\n", "Força: ", forca2, "\n", "Armadura: ", armor2, "\n", "HP: ", Currenthp2, "\n");
+        console.log("Vencedor: ", char);
+    }
+    else{
+        console.log("Nome: ", char, "\n", "Força: ", forca1, "\n", "Armadura: ", armor1, "\n", "HP: " ,Currenthp1, "\n", "Nome: ", char2, "\n", "Força: ", forca2, "\n", "Armadura: ", armor2, "\n", "HP: ", Currenthp2, "\n");
+        console.log("Vencedor: ", char);
+    }
+
 }
